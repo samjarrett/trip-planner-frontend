15b6ed4a2bf990799507de809db21656343e1e35
Sam Jarrett <samstah@gmail.com>
2019-09-09 23:08:34 +1000

    Add functionality to leave notes

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 import Avatar from './Avatar';
 import withUser from '../withUser';
+
+import GET_IDEA_QUERY from '../queries/get-idea';
+import CREATE_NOTE_MUTATION from '../queries/create-note';
 
 
 const Wrapper = styled(Paper)`
@@ -24,18 +28,57 @@ const CancelWrapper = styled.div`
   text-align: right;
 `;
 
-const CreateNote = ({ user }) => (
-  <Wrapper>
-    <Avatar user={user} />
-    <NoteForm>
-      <TextField fullWidth={true} label="Note" name="body" type="text" multiline={true} variant="outlined" />
-      <CancelWrapper>
-        <Button color="primary" variant="outlined" size="small" type="submit">
-          Save
-        </Button>
-      </CancelWrapper>
-    </NoteForm>
-  </Wrapper>
-);
+const DEFAULT_STATE = {
+  body: ''
+};
+
+const CreateNote = ({ user, ideaId }) => {
+  const [state, setState] = useState(DEFAULT_STATE);
+  const [createNote] = useMutation(CREATE_NOTE_MUTATION, {
+    update(cache, { data: { createNote } }) {
+      const { idea } = cache.readQuery({ query: GET_IDEA_QUERY, variables: { id: ideaId } });
+      const notes = [createNote.note].concat(idea.notes);
+
+      cache.writeQuery({
+        query: GET_IDEA_QUERY,
+        variables: { id: ideaId },
+        data: { idea: {
+          ...idea,
+          notes
+        } },
+      });
+
+      setState(DEFAULT_STATE);
+    }
+  });
+
+  const handleChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  }
+
+  return (
+    <Wrapper>
+      <Avatar user={user} />
+      <NoteForm onSubmit={e => {
+        e.preventDefault();
+        createNote({
+          variables: { input: Object.assign({}, state, { idea: ideaId }) }
+        });
+      }}>
+        <TextField fullWidth={true} label="Note" name="body" type="text" multiline={true} variant="outlined" value={state.body} onChange={handleChange} />
+        <CancelWrapper>
+          <Button color="primary" variant="outlined" size="small" type="submit">
+            Save
+          </Button>
+        </CancelWrapper>
+      </NoteForm>
+    </Wrapper>
+  );
+};
 
 export default withUser(CreateNote);
