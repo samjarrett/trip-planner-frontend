72addd2e02fcd078c696a7751b1aed376d87c11a
Sam Jarrett <samstah@gmail.com>
2019-09-09 20:42:26 +1000

    If a user isn't logged in, redirect them to the root

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import withUser from '../withUser';
 import ShowPlan from './ShowPlan';
 import CreateIdea from './CreateIdea';
 import ShowIdea from './ShowIdea';
 
-const Plan = ({ match }) => (
-  <>
-    <Route path={`${match.path}/new`} component={CreateIdea} />
-    <Route path={`${match.path}/:idea`} component={ShowIdea} />
-    <Route exact path={match.path} component={ShowPlan} />
-  </>
-);
-export default Plan;
+const Plan = ({ match, user }) => {
+
+  if (!user) {
+    return (
+      <Redirect to="/" />
+    );
+  }
+
+  return (
+    <>
+      <Route path={`${match.path}/new`} component={CreateIdea} />
+      <Route path={`${match.path}/:idea`} component={ShowIdea} />
+      <Route exact path={match.path} component={ShowPlan} />
+    </>
+  );
+};
+export default withUser(Plan);
+
