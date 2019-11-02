dcdf3f394d9f9ecb5f5a31118c661dc52009e015
Sam Jarrett <samstah@gmail.com>
2019-11-02 15:18:14 +1100

    better handle avatars

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import MUIAvatar from '@material-ui/core/Avatar';
 import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_AVATAR_VALIDITY = gql`
+  {
+    brokenAvatars @client {
+      gravatarHash
+    }
+  }
+`;
 
 function hashCode(str) { // java String#hashCode
   let hash = 0;
@@ -22,13 +32,33 @@ const AvatarButton = styled(MUIAvatar)`
   background-color: #${props => intToRGB(hashCode(props.hash))} !important;
 `;
 
-const Avatar = ({ user }) => (
-  <AvatarButton
-    title={user.firstName}
-    hash={user.gravatarHash}
-    >
-      {user.firstName[0]}
-  </AvatarButton>
-);
+const Avatar = ({ user }) => {
+  const { data, client } = useQuery(GET_AVATAR_VALIDITY);
+  console.log(data);
+
+  const brokenAvatar = data.brokenAvatars.find((element) => {
+    return element.gravatarHash === user.gravatarHash;
+  }) || null;
+
+  const errorHandler = () => {
+    const brokenAvatars = data.brokenAvatars.concat([{
+      gravatarHash: user.gravatarHash,
+      __typename: 'BrokenAvatar'
+    }]);
+    client.writeData({ data: { brokenAvatars } });
+  }
+
+  return (
+    <AvatarButton
+      title={user.firstName}
+      hash={user.gravatarHash}
+      src={brokenAvatar === null ? `https://www.gravatar.com/avatar/${user.gravatarHash}?s=128&d=404` : null}
+      alt={user.firstName[0]}
+      onError={errorHandler}
+      >
+        {user.firstName[0]}
+    </AvatarButton>
+  );
+};
 
 export default Avatar;
